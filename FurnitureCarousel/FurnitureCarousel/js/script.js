3ebd6d157b47e7d87577b8246dd89044b95e72f7
Aman123axr <113253972+Aman123axr@users.noreply.github.com>
2023-12-01 16:29:02 +0530

    Add files via upload

---
+++ FurnitureCarousel/FurnitureCarousel/js/script.js
@@ -0,0 +1,35 @@
+const imgs = document.querySelectorAll(".container img");
+const dots = document.querySelectorAll(".dot i");
+const leftArrow = document.querySelector(".arrow-left");
+const rightArrow = document.querySelector(".arrow-right");
+
+let currentIndex = 0;
+let time = 5000; // default time for auto slideshow
+
+const defClass = (startPos, index) => {
+  for (let i = startPos; i < imgs.length; i++) {
+    imgs[i].style.display = "none";
+  }
+  imgs[index].style.display = "block";
+};
+
+defClass(1, 0);
+
+leftArrow.addEventListener("click", function(){
+  currentIndex <= 0 ? currentIndex = imgs.length-1 : currentIndex--;
+  defClass(0, currentIndex);
+});
+
+rightArrow.addEventListener("click", function(){
+  currentIndex >= imgs.length-1 ? currentIndex = 0 : currentIndex++;
+  defClass(0, currentIndex);
+});
+
+const startAutoSlide = () => {
+  setInterval(() => {
+    currentIndex >= imgs.length-1 ? currentIndex = 0 : currentIndex++;
+    defClass(0, currentIndex);
+  }, time);
+};
+
+startAutoSlide(); // Start the slideshow
